8cc5669cd14ead2879cd038826c19f28b46f982a
Waseem Aslam <w.aslam@rocketmail.com>
2020-07-04 17:22:04 +0100

    _____________________ Commit | Project nearly done |         ---------------------

--- mainFiles/js/main.js
+++ mainFiles/js/main.js
@@ -9,3 +9,19 @@ let numbers = {
 
 };
 document.write("numbers");
+/* This code isn't mine but I am going to use it anyway (it is GreenBayRules' at Github)
+
+ */
+function randomNumber (max, min) {
+  return (Math.floor(Math.random()*(max-min))+1)+min;
+}
+
+function onClick() {
+
+  document.getElementById("output").style.cssText = "font-size:1px;transition: all 0.5s ease;";
+  var max = parseInt(document.getElementById("maxInput").value);
+  var min = parseInt(document.getElementById("minInput").value);
+  var rand = randomNumber(max,min);
+  document.getElementById("output").innerHTML = rand;
+  document.getElementById("output").style.cssText = "font-size:30px;transition: all 1s ease;";
+}
